8f7e3a5a860cb22d0f8dfb4bb65cf13e288d8bfb
thivy <thivyakanth@gmail.com>
2024-08-02 15:59:07 +1000

    add

--- frontend/image-generator/app/page.tsx
+++ frontend/image-generator/app/page.tsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <main className="flex-col min-h-screen flex items-center justify-center">
       <div className="rounded-2xl aspect-square shadow-md border-2 border-slate-50 w-full overflow-hidden relative bg-slate-300">
-        <div className="w-full h-full absolute left-0 top-0 text-2xl md:text-4xl tracking-tight flex items-center justify-center bg-[url('./poster.png')] bg-cover bg-center">
-          <span className="font-bold">Hello!</span>
-        </div>
+        <div className="w-full h-full absolute left-0 top-0 text-2xl md:text-4xl tracking-tight flex items-center justify-center bg-[url('./poster.png')] bg-cover bg-center"></div>
       </div>
     </main>
   );
