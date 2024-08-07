5b60e7e2da2b046a0206d99b48cc96519ef1270e
thivy <thivyakanth@gmail.com>
2024-08-07 16:42:51 +1000

    add

--- frontend/image-generator/app/images/page.tsx
+++ frontend/image-generator/app/images/page.tsx
@@ -1,13 +1,13 @@
 import { fullImageUrl, getAllImages } from "@/features/actions";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function ImagesHome() {
   const images = await getAllImages();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 flex-1 gap-4 space-y-5 items-center justify-center flex-col pb-4">
-      {images.map(async (image) => {
-        console.log(await fullImageUrl(image.id));
+      {images.reverse().map(async (image) => {
         return (
           <main className="flex-col flex gap-2" key={image.id}>
             <div className="text-sm text-slate-500">{image.userId}</div>
